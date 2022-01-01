f5315ff11b465803a24734e014e65b832c492c7a
ridoansaleh <ridoansalehnasution98@gmail.com>
2022-01-01 17:28:54 +0700

    add departure date, arrival date, arrival modal, and passenger & cabin modal

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const departureAirport = document.getElementById("departure-airport");
 const departureSelect = document.getElementById("departure-select");
+const arrivalAirport = document.getElementById("arrival-airport");
+const arrivalSelect = document.getElementById("arrival-select");
+const passengerAndCabinText = document.getElementById(
+  "passenger-and-cabin-text"
+);
+const passengerAndCabin = document.getElementById("passenger-and-cabin");
 
 departureAirport.addEventListener("click", () => {
   departureSelect.style.display = "block";
@@ -8,4 +14,22 @@ departureAirport.addEventListener("click", () => {
 const closeDepartureModal = (e) => {
   e.stopPropagation();
   departureSelect.style.display = "none";
+};
+
+arrivalAirport.addEventListener("click", () => {
+  arrivalSelect.style.display = "block";
+});
+
+const closeArrivalModal = (e) => {
+  e.stopPropagation();
+  arrivalSelect.style.display = "none";
+};
+
+passengerAndCabinText.addEventListener("click", () => {
+  passengerAndCabin.style.display = "block";
+});
+
+const closePassengerAndCabinModal = (e) => {
+  e.stopPropagation();
+  passengerAndCabin.style.display = "none";
 };
